5b3e2ee52f2fcbb8caddba1c1765f728ff9f7a4d
DOdrums <dirkornee@hotmail.com>
2022-05-19 14:07:35 +0000

    practice more with restructuring

--- destructuring.js
+++ destructuring.js
@@ -21,6 +21,34 @@ let {mike, jill, alicia} = jobs;
 console.log(mike, jill, alicia);
 
 // Destructuring subsets
-let languages = ["english", "french", "spanish", "german", "japanse"];
-let [johnNative, johnSecondary] = langauges
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+let [, , maryNative, marySeconday] = languages;
+console.log(johnNative, johnSecondary);
+console.log(maryNative, marySeconday);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+
+let {firstLanguage, thirdLanguage} = languages2;
+console.log(firstLanguage, thirdLanguage);
+
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondfavorite, ...others] = fruits;
+console.log(favorite, secondfavorite, others);
+
+let favoriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let {brian, anna, ...rest} = favoriteFoods;
+console.log(brian)
+console.log(anna)
+console.log(rest)
