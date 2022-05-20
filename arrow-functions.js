38eb491a7a66cfec598a37bf9dcea0415e744d84
DOdrums <dirkornee@hotmail.com>
2022-05-20 13:20:52 +0000

    add arrow functions js scritp to this repo taken from arrow functions repo

---
+++ arrow-functions.js
@@ -0,0 +1,34 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node arrow-functions.js in the terminal
+ */
+
+// Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
+
+// Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+    // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum);
+
+// Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let nummertje = addTwoNumbers2(4, 9)
+console.log(nummertje)
+
+// Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('hello');
+sayHello();
+
+// Returning Multiple Lines
